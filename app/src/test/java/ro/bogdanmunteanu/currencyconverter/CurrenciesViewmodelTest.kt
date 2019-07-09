2c925e0602331bb2824c7f6f580a0702b48e4465
bogdanmunteanu <bogdanmunteanu25@gmail.com>
2019-07-09 22:16:11 +0300

    Modified unit test for viewmodel

--- app/src/test/java/ro/bogdanmunteanu/currencyconverter/CurrenciesViewmodelTest.kt
+++ app/src/test/java/ro/bogdanmunteanu/currencyconverter/CurrenciesViewmodelTest.kt
@@ -2,20 +2,22 @@ package ro.bogdanmunteanu.currencyconverter
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
-import io.mockk.MockKAnnotations
+import io.mockk.*
 import io.mockk.impl.annotations.RelaxedMockK
-import io.mockk.mockk
-import io.mockk.verify
+import io.reactivex.subscribers.DisposableSubscriber
 import org.junit.Before
 import org.junit.ClassRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito
-import org.mockito.MockitoAnnotations
+import org.mockito.*
+import org.mockito.ArgumentCaptor.forClass
+import org.mockito.Mockito.anyList
+import org.mockito.Mockito.times
 import org.mockito.junit.MockitoJUnitRunner
 import ro.bogdanmunteanu.currencyconverter.data.di.RevolutApiService
 import ro.bogdanmunteanu.currencyconverter.data.repository.RevolutServiceRepository
+import ro.bogdanmunteanu.currencyconverter.utils.CurrencyDisposable
 import ro.bogdanmunteanu.currencyconverter.viewmodel.CurrenciesViewModel
 import java.lang.Exception
 
@@ -30,6 +32,15 @@ class CurrenciesViewModelTest {
     lateinit var repository: RevolutServiceRepository
 
     @RelaxedMockK
+    lateinit var stateOberver: Observer<CurrenciesViewModel.State>
+
+    @RelaxedMockK
+    lateinit var currenciesObserver: Observer<List<Any>>
+
+    @RelaxedMockK
+    lateinit var disposable: CurrencyDisposable
+
+    @RelaxedMockK
     lateinit var viewModel: CurrenciesViewModel
 
     companion object {
@@ -37,6 +48,8 @@ class CurrenciesViewModelTest {
         @JvmField
         val schedulers = RxImmediateSchedulerRule()
     }
+
+
 
 
     @Rule
@@ -51,17 +64,19 @@ class CurrenciesViewModelTest {
         MockKAnnotations.init(this)
         repository = RevolutServiceRepository(service)
         viewModel = CurrenciesViewModel(repository)
+
     }
 
     @Test
     fun viewModelTest() {
-//not working
+        //not working
         val baseCurrency: String = "EUR"
-        val stateObserver = mockk<Observer<CurrenciesViewModel.State>>(relaxed = true)
-
-        Mockito.`when`(viewModel.getLiveCurrencies(baseCurrency)).thenAnswer {}
-        verify(exactly = 1){ stateObserver.onChanged(CurrenciesViewModel.State.IN_PROGRESS) }
-
-
+        viewModel.fetchState.observeForever(stateOberver)
+        every { viewModel.getLiveCurrencies(baseCurrency)} just Runs
+        io.mockk.verify(exactly = 1) { viewModel.getLiveCurrencies(baseCurrency) }
+        io.mockk.verify{currenciesObserver.onChanged(ArgumentMatchers.anyList())}
     }
+
+
+
 }
